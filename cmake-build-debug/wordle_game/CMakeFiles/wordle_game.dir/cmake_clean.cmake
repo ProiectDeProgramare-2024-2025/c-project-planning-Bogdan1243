3f1e15d6eb31611b0261a75e52ea664e8e7a7bdf
Bogdan1243 <bogdan.mihailescu04@e-uvt.ro>
2025-06-06 16:46:15 +0300

    Add files via upload

---
+++ cmake-build-debug/wordle_game/CMakeFiles/wordle_game.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/wordle_game.dir/check_guess.cpp.obj"
+  "CMakeFiles/wordle_game.dir/check_guess.cpp.obj.d"
+  "CMakeFiles/wordle_game.dir/interface.cpp.obj"
+  "CMakeFiles/wordle_game.dir/interface.cpp.obj.d"
+  "CMakeFiles/wordle_game.dir/main.cpp.obj"
+  "CMakeFiles/wordle_game.dir/main.cpp.obj.d"
+  "libwordle_game.dll.a"
+  "wordle_game.exe"
+  "wordle_game.exe.manifest"
+  "wordle_game.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/wordle_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/wordle_login/CMakeFiles/wordle_login.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Bogdan/CLionProjects/WordleeClone/wordle_login/login_manager.cpp" "wordle_login/CMakeFiles/wordle_login.dir/login_manager.cpp.obj" "gcc" "wordle_login/CMakeFiles/wordle_login.dir/login_manager.cpp.obj.d"
+  "C:/Users/Bogdan/CLionProjects/WordleeClone/wordle_login/main.cpp" "wordle_login/CMakeFiles/wordle_login.dir/main.cpp.obj" "gcc" "wordle_login/CMakeFiles/wordle_login.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
